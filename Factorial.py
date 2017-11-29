da220135cdf2036f1939af35ca0d9b53c9806874
muddulur <nikhillovesapple@gmail.com>
2017-11-29 11:02:26 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,13 @@
+def fact(x):
+
+    if x<=1:
+
+        return x
+
+    else:
+
+        return x*fact(x-1)
+
+entered=int(input("enter the num : "))
+
+print("the factorial of given num is: ",fact(entered))
