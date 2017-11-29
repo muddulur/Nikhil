2ec5f098cfa45bce33bbcd798a1369cef22094cb
muddulur <nikhillovesapple@gmail.com>
2017-11-29 10:55:41 +0530

    Add files via upload

---
+++ Sum of natural numbers.py
@@ -0,0 +1,19 @@
+def sum(n):
+
+    if n<=1:
+
+        return n
+
+    else:
+
+        return n+sum(n-1)
+
+num=int(input("enter the number:"))
+
+if num<=0:
+
+    print("enter the positive number:")
+
+else:
+
+    print("the sum of natural numbers is: ",sum(num))
