d41dee97405efe44f5fb85e208b70d6b2ab4c7b3
muddulur <nikhillovesapple@gmail.com>
2017-11-29 10:02:18 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,15 @@
+def fib_series(x):
+
+    if x<=1:
+
+        return x;
+
+    else:
+
+        return (fib_series(x-1)+fib_series(x-2));
+
+no_of_steps=int(input("enter the no of steps you want to see the fib series: "));
+
+for i in range(no_of_steps):
+
+    print(fib_series(i));
---
+++ Finding Factors.py
@@ -0,0 +1,13 @@
+input_num=int(input("Enter the number to which factors are needed: "));
+
+for i in range(1,input_num):
+
+    factor=input_num%i;
+
+    if factor == 0:
+
+        print (i,"\t");
+
+    else:
+
+        continue;
